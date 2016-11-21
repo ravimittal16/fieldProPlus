d86b456e825e39da82422361b0f438bf7522c3d2
Ravi Mittal <ravimittal16@gmail.com>
2016-11-21 21:03:08 +0530

    updated

--- www/js/map/map-controller.js
+++ www/js/map/map-controller.js
@@ -1,10 +1,13 @@
 (function () {
     "use strict";
+    var defaultColor = "#000000";
     var defCordinates = { x: 44.31127, y: -92.67851, xcom: -92.67851, ycom: 44.3112679 };
-    function initController($scope, $cordovaGeolocation, $timeout, $ionicModal, mapFactory, sharedDataFactory) {
+    var mapMarkers = [];
+    var markers = [];
+    function initController($scope, $cordovaGeolocation, $timeout, $ionicModal, mapFactory,
+        sharedDataFactory, fpmUtilitiesFactory) {
         var vm = this;
         var options = { timeout: 10000, enableHighAccuracy: true };
-        console.log($cordovaGeolocation)
         var mapOptions = {
             center: null,
             panControl: false,
@@ -30,35 +33,147 @@
 
         vm.filtersApplied = false;
         function onFiltersClicked() {
-            console.log("HELLO WORLD");
             if (vm.filtersModal) {
                 vm.filtersModal.show();
             }
         }
         function clearFilters() {
             if (vm.filtersModal) {
+                var uncheckedUsers = _.where(vm.mapData.users, { isChecked: false });
+                _.forEach(uncheckedUsers, function (u) {
+                    u.isChecked = true;
+                });
                 vm.filtersModal.hide();
             }
         }
         function applyFilters() {
-            
-         }
+            var checkedUsers = _.where(vm.mapData.users, { isChecked: true });
+            console.log("checkedUsers", checkedUsers);
+        }
+        function refreshMapClicked() {
+            getMapData(true);
+        }
         vm.events = {
             onFiltersClicked: onFiltersClicked,
             clearFilters: clearFilters,
-            applyFilters: applyFilters
+            applyFilters: applyFilters,
+            refreshMapClicked: refreshMapClicked
         };
         vm.mapData = {
-            users: []
+            users: [],
+            orders: []
         };
-        function getMapData() {
-            mapFactory.getMapData().then(function (response) {
-                if (response) {
-                    vm.mapData.users = response.users;
+        function addHandler(m, o) {
+            window.google.maps.event.addListener(m, "click", function () {
+                if (o.barCode !== "[HOME]") {
+                    if (o.numFromTechnician !== 0) {
+                        //$state.go("orderEdit", { barCode: o.barCode, technicianNum: o.numFromTechnician, src: "map" });
+                    }
                 }
             });
         }
+        var orderCounter = 1;
+        function buildMap() {
+            var mapdata = vm.mapData;
+            if (mapMarkers && mapMarkers.length > 0) {
+                $.each(mapMarkers, function (i, l) {
+                    var marker = null;
+                    var myLatLng = new window.google.maps.LatLng(l.y, l.x);
+                    if (l.isHome === true) {
+                        marker = new window.google.maps.Marker({
+                            position: myLatLng,
+                            map: vm.map,
+                            icon: {
+                                url: l.imageurl,
+                                size: new window.google.maps.Size(24, 24),
+                                origin: new window.google.maps.Point(0, 0),
+                                anchor: new window.google.maps.Point(0, 24)
+                            },
+                            shape: {
+                                coord: [1, 1, 1, 20, 18, 20, 18, 1],
+                                type: 'poly'
+                            }
+                        });
+                        addHandler(marker, l);
+                    } else {
+                        if (l.isAssigned === true) {
+                            marker = new window.google.maps.Marker({
+                                position: myLatLng,
+                                map: vm.map,
+                                labelContent: orderCounter,
+                                labelAnchor: new window.google.maps.Point(0, 7),
+                                labelClass: "labels",
+                                labelStyle: { opacity: 0.75 },
+                                icon: {
+                                    path: window.google.maps.SymbolPath.CIRCLE,
+                                    scale: l.scale || 6,
+                                    fillColor: l.color || defaultColor,
+                                    strokeColor: l.color || defaultColor
+                                }
+                            });
+                            orderCounter += 1;
+                            markers.push({ detail: l, marker: marker, technician: l.technician, havingMap: true, isassigned: l.isAssigned, start: (l.start != null ? new Date(l.start) : null), isSpecialMarker: true, numFromTechnician: l.NumFromTechnician });
+                        } else {
+                            marker = new window.google.maps.Marker({
+                                position: myLatLng,
+                                map: vm.map,
+                                icon: {
+                                    path: window.google.maps.SymbolPath.CIRCLE,
+                                    scale: l.scale || 6,
+                                    fillColor: l.color || defaultColor,
+                                    strokeColor: l.color || defaultColor
+                                }
+                            });
+                            markers.push({ detail: l, marker: marker, technician: l.technician, havingMap: true, isassigned: l.isAssigned, start: (l.start != null ? new Date(l.start) : null), isSpecialMarker: false, numFromTechnician: l.NumFromTechnician });
+                        }
+                        addHandler(marker, l);
+                    }
+                    if ((mapMarkers.length - 1) === i) {
+
+                    }
+                });
+            }
+        }
+        function updateMapMarkersArray() {
+            mapMarkers = [];
+            $.each(vm.mapData.orders, function (i, e) {
+                var color = defaultColor;
+                if (vm.mapData.users && e.technicianNum != null) {
+                    var cRecd = _.findWhere(vm.mapData.users, { userId: e.technicianNum });
+                    if (cRecd) {
+                        color = cRecd.userColor || defaultColor;
+                    }
+                }
+                var isWorkAssigned = e.scheduledStartDateTime !== null && e.scheduledFinishDateTime !== null;
+                if ((Math.abs(e.coordinateX + 98.908) > 0.1) && (Math.abs(e.coordinateY - 39.45) > 0.1)) {
+                    mapMarkers.push({
+                        x: e.coordinateX, y: e.coordinateY, barCode: e.barcode, isHome: false, color: color,
+                        scale: color === defaultColor ? 4 : 6, isAssigned: isWorkAssigned,
+                        technician: e.technicianNum, start: e.scheduledStartDateTime,
+                        numFromTechnician: e.numFromTechnicianSchedule
+                    });
+                }
+                if (i === vm.mapData.orders.length - 1) {
+                    buildMap();
+                }
+            });
+        }
+        function getMapData(forceGet) {
+
+            fpmUtilitiesFactory.showLoading().then(function () {
+                mapFactory.getMapData(forceGet).then(function (response) {
+                    if (response) {
+                        vm.mapData.users = response.users;
+                        vm.mapData.orders = response.orders;
+                        if (angular.isDefined(vm.mapData) && vm.mapData.orders.length > 0) {
+                            updateMapMarkersArray();
+                        }
+                    }
+                }).finally(fpmUtilitiesFactory.hideLoading);
+            });
+        }
         function getIniitialData() {
+            markers = [];
             mapOptions.center = new window.google.maps.LatLng(defCordinates.y, defCordinates.x);
             sharedDataFactory.getIniitialData().then(function (response) {
                 vm.iniitialData = response;
@@ -67,19 +182,31 @@
                     mapOptions.center = new window.google.maps.LatLng(cn.coordinateY || defCordinates.y, cn.coordinateX || defCordinates.x);
                     mapOptions.zoom = cn.zoomDepth || 8;
                     vm.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+                    mapMarkers.push({ x: cn.coordinateX || defCordinates.x, y: cn.coordinateY || defCordinates.y, imageurl: "../images/markers/home.png", barCode: "[HOME]", isHome: true, color: defaultColor, technician: '', isAssigned: true, start: null, numFromTechnician: 0 });
                 } else {
                     mapOptions.center = new window.google.maps.LatLng(defCordinates.y, defCordinates.x);
                     vm.map = new google.maps.Map(document.getElementById("map"), mapOptions);
                 }
-            }).finally(getMapData);
+
+            }).finally(function () {
+                getMapData(false);
+            });
         }
         function activateController() {
             getIniitialData();
         }
 
-        activateController();
+        $scope.$on("$ionicView.afterEnter", function (e, data) {
+            activateController();
+        });
+        $scope.$on("$destroy", function (event) { 
+            vm.map = null;
+            mapMarkers = [];
+            markers = [];
+        });
     }
 
-    initController.$inject = ["$scope", "$cordovaGeolocation", "$timeout", "$ionicModal", "map-factory", "shared-data-factory"];
+    initController.$inject = ["$scope", "$cordovaGeolocation", "$timeout", "$ionicModal",
+        "map-factory", "shared-data-factory", "fpm-utilities-factory"];
     angular.module("fpm").controller("map-controller", initController);
 })();
--- www/js/work-orders/work-orders-factory.js
+++ www/js/work-orders/work-orders-factory.js
@@ -1,152 +1,152 @@
-(function () {
-  "use strict";
+(function() {
+    "use strict";
 
-  function initFactory($q, $cacheFactory, apiBaseFactory, sharedDataFactory) {
-    var apibaseurl = "api/workorders/"
-    var cache = $cacheFactory("orderCache");
-    var dashboardDataKeyName = "dashboardData";
+    function initFactory($q, $cacheFactory, apiBaseFactory, sharedDataFactory) {
+        var apibaseurl = "api/workorders/"
+        var cache = $cacheFactory("orderCache");
+        var dashboardDataKeyName = "dashboardData";
 
-    function getMobileDashboard(forceGet, initialData) {
-      forceGet = forceGet === null ? false : forceGet;
-      if (forceGet === true) {
-        cache.remove(dashboardDataKeyName);
-      }
-      var orders = cache.get(dashboardDataKeyName);
-      if (angular.isDefined(orders) && orders) {
-        return $q.when(orders);
-      } else {
-        return apiBaseFactory.get(apibaseurl + "GetMobileDashboard").then(function (response) {
-          cache.put(dashboardDataKeyName, response);
-          return response;
-        });
-      }
+        function getMobileDashboard(forceGet, initialData) {
+            forceGet = forceGet === null ? false : forceGet;
+            if (forceGet === true) {
+                cache.remove(dashboardDataKeyName);
+            }
+            var orders = cache.get(dashboardDataKeyName);
+            if (angular.isDefined(orders) && orders) {
+                return $q.when(orders);
+            } else {
+                return apiBaseFactory.get(apibaseurl + "GetMobileDashboard").then(function(response) {
+                    cache.put(dashboardDataKeyName, response);
+                    return response;
+                });
+            }
+        }
+
+        function getBarcodeDetails(barcode) {
+            return apiBaseFactory.get(apibaseurl + "GetBarcodeDetails?barcode=" + barcode);
+        }
+
+        function deleteProduct(barcode, num) {
+            return apiBaseFactory.get(apibaseurl + "DeleteProduct?barcode=" + barcode + "&productNum=" + num);
+        }
+
+        function updateProduct(model) {
+            return apiBaseFactory.post(apibaseurl + "UpdateProduct?fromMobile=true", model);
+        }
+
+        function searchProduct(searchValue, alphabet) {
+            return apiBaseFactory.get(apibaseurl + "GetProductsLists?searchPattern=" + searchValue + "&alphabet=" + alphabet);
+        }
+
+        function addProduct(model) {
+            return apiBaseFactory.post(apibaseurl + "AddProductsToOrder?fromMobile=true", model);
+        }
+
+        function getBarcodeInvoiceAndProductDetails(barcode) {
+            return apiBaseFactory.get(apibaseurl + "GetBarcodeInvoiceAndProductDetails?barcode=" + barcode);
+        }
+
+        function saveJsonSignForBarcode(obj) {
+            return apiBaseFactory.post(apibaseurl + "SaveJsonSignForBarcode", obj);
+        }
+
+        function sendInvoiceMail(model) {
+            return apiBaseFactory.post(apibaseurl + "SendInvoiceMail", model);
+        }
+
+        function getImagesList(barcode) {
+            return apiBaseFactory.get(apibaseurl + "GetImagesList?barcode=" + barcode);
+        }
+
+        function getUploadedDocuments(barcode) {
+            return apiBaseFactory.get(apibaseurl + "GetUploadedDocuments?barcode=" + barcode);
+        }
+
+        function uploadFile(model) {
+            return apiBaseFactory.post(apibaseurl + "UploadFile", model);
+        }
+
+        function createEntity() {
+            var entity = cache.get("workOrderEntity");
+            if (angular.isDefined(entity) && entity) {
+                return $q.when(entity);
+            } else {
+                return apiBaseFactory.get(apibaseurl + "CreateEntity").then(function(response) {
+                    cache.put("workOrderEntity", response);
+                    return response;
+                });
+            }
+        }
+
+        function createWorkOrder(model) {
+            return apiBaseFactory.post(apibaseurl + "CreateWorkOrder", model);
+        }
+
+        function getBarCodeNumber() {
+            return apiBaseFactory.get(apibaseurl + "GetBarCodeNumber");
+        }
+
+        function updateWorkOrderMobile(model) {
+            return apiBaseFactory.post(apibaseurl + "UpdateWorkOrderMobile", model);
+        }
+
+        function updateJobStatus(sch) {
+            sch.clientTime = new Date();
+            return apiBaseFactory.post(apibaseurl + "UpdateJobStatus", sch);
+        }
+
+        function updateSchedule(schedule) {
+            return apiBaseFactory.post(apibaseurl + "UpdateSchedule?fromMobile=true", schedule);
+        }
+
+        function addWorkOrderSchedule(schedule) {
+            return apiBaseFactory.post(apibaseurl + "AddWorkOrderSchedule", schedule);
+        }
+
+        function updateOrderProduct(product) {
+            return apiBaseFactory.post(apibaseurl + "UpdateOrderProduct", product);
+        }
+        var workOrderCalendarKeyName = "workorder:calendar";
+        function getDatewiseEvents(forceGet) {
+            forceGet = forceGet === null ? false : forceGet;
+            if (forceGet === true) {
+                cache.remove(workOrderCalendarKeyName);
+            }
+            var calenderSchedules = cache.get(workOrderCalendarKeyName);
+            if (angular.isDefined(calenderSchedules) && calenderSchedules) {
+                return $q.when(calenderSchedules);
+            } else {
+                return apiBaseFactory.get("api/Scheduler/GetMyCalender?fromMobile=true").then(function(response) {
+                    cache.put(workOrderCalendarKeyName, response);
+                    return response;
+                });
+            }
+        }
+
+        return {
+            getMobileDashboard: getMobileDashboard,
+            getBarcodeDetails: getBarcodeDetails,
+            deleteProduct: deleteProduct,
+            updateProduct: updateProduct,
+            searchProduct: searchProduct,
+            addProduct: addProduct,
+            getBarcodeInvoiceAndProductDetails: getBarcodeInvoiceAndProductDetails,
+            saveJsonSignForBarcode: saveJsonSignForBarcode,
+            sendInvoiceMail: sendInvoiceMail,
+            getImagesList: getImagesList,
+            getUploadedDocuments: getUploadedDocuments,
+            uploadFile: uploadFile,
+            createEntity: createEntity,
+            createWorkOrder: createWorkOrder,
+            getBarCodeNumber: getBarCodeNumber,
+            updateWorkOrderMobile: updateWorkOrderMobile,
+            updateJobStatus: updateJobStatus,
+            updateSchedule: updateSchedule,
+            addWorkOrderSchedule: addWorkOrderSchedule,
+            updateOrderProduct: updateOrderProduct,
+            getDatewiseEvents: getDatewiseEvents
+        };
     }
-
-    function getBarcodeDetails(barcode) {
-      return apiBaseFactory.get(apibaseurl + "GetBarcodeDetails?barcode=" + barcode);
-    }
-
-    function deleteProduct(barcode, num) {
-      return apiBaseFactory.get(apibaseurl + "DeleteProduct?barcode=" + barcode + "&productNum=" + num);
-    }
-
-    function updateProduct(model) {
-      return apiBaseFactory.post(apibaseurl + "UpdateProduct?fromMobile=true", model);
-    }
-
-    function searchProduct(searchValue, alphabet) {
-      return apiBaseFactory.get(apibaseurl + "GetProductsLists?searchPattern=" + searchValue + "&alphabet=" + alphabet);
-    }
-
-    function addProduct(model) {
-      return apiBaseFactory.post(apibaseurl + "AddProductsToOrder?fromMobile=true", model);
-    }
-
-    function getBarcodeInvoiceAndProductDetails(barcode) {
-      return apiBaseFactory.get(apibaseurl + "GetBarcodeInvoiceAndProductDetails?barcode=" + barcode);
-    }
-
-    function saveJsonSignForBarcode(obj) {
-      return apiBaseFactory.post(apibaseurl + "SaveJsonSignForBarcode", obj);
-    }
-
-    function sendInvoiceMail(model) {
-      return apiBaseFactory.post(apibaseurl + "SendInvoiceMail", model);
-    }
-
-    function getImagesList(barcode) {
-      return apiBaseFactory.get(apibaseurl + "GetImagesList?barcode=" + barcode);
-    }
-
-    function getUploadedDocuments(barcode) {
-      return apiBaseFactory.get(apibaseurl + "GetUploadedDocuments?barcode=" + barcode);
-    }
-
-    function uploadFile(model) {
-      return apiBaseFactory.post(apibaseurl + "UploadFile", model);
-    }
-
-    function createEntity() {
-      var entity = cache.get("workOrderEntity");
-      if (angular.isDefined(entity) && entity) {
-        return $q.when(entity);
-      } else {
-        return apiBaseFactory.get(apibaseurl + "CreateEntity").then(function (response) {
-          cache.put("workOrderEntity", response);
-          return response;
-        });
-      }
-    }
-
-    function createWorkOrder(model) {
-      return apiBaseFactory.post(apibaseurl + "CreateWorkOrder", model);
-    }
-
-    function getBarCodeNumber() {
-      return apiBaseFactory.get(apibaseurl + "GetBarCodeNumber");
-    }
-
-    function updateWorkOrderMobile(model) {
-      return apiBaseFactory.post(apibaseurl + "UpdateWorkOrderMobile", model);
-    }
-
-    function updateJobStatus(sch) {
-      sch.clientTime = new Date();
-      return apiBaseFactory.post(apibaseurl + "UpdateJobStatus", sch);
-    }
-
-    function updateSchedule(schedule) {
-      return apiBaseFactory.post(apibaseurl + "UpdateSchedule?fromMobile=true", schedule);
-    }
-
-    function addWorkOrderSchedule(schedule) {
-      return apiBaseFactory.post(apibaseurl + "AddWorkOrderSchedule", schedule);
-    }
-
-    function updateOrderProduct(product) {
-      return apiBaseFactory.post(apibaseurl + "UpdateOrderProduct", product);
-    }
-    var workOrderCalendarKeyName = "workorder:calendar";
-    function getDatewiseEvents(forceGet) {
-      forceGet = forceGet === null ? false : forceGet;
-      if (forceGet === true) {
-        cache.remove(workOrderCalendarKeyName);
-      }
-      var calenderSchedules = cache.get(workOrderCalendarKeyName);
-      if (angular.isDefined(calenderSchedules) && calenderSchedules) {
-        return $q.when(calenderSchedules);
-      } else {
-        return apiBaseFactory.get("api/Scheduler/GetMyCalender?fromMobile=true").then(function (response) {
-          cache.put(workOrderCalendarKeyName, response);
-          return response;
-        });
-      }
-    }
-
-    return {
-      getMobileDashboard: getMobileDashboard,
-      getBarcodeDetails: getBarcodeDetails,
-      deleteProduct: deleteProduct,
-      updateProduct: updateProduct,
-      searchProduct: searchProduct,
-      addProduct: addProduct,
-      getBarcodeInvoiceAndProductDetails: getBarcodeInvoiceAndProductDetails,
-      saveJsonSignForBarcode: saveJsonSignForBarcode,
-      sendInvoiceMail: sendInvoiceMail,
-      getImagesList: getImagesList,
-      getUploadedDocuments: getUploadedDocuments,
-      uploadFile: uploadFile,
-      createEntity: createEntity,
-      createWorkOrder: createWorkOrder,
-      getBarCodeNumber: getBarCodeNumber,
-      updateWorkOrderMobile: updateWorkOrderMobile,
-      updateJobStatus: updateJobStatus,
-      updateSchedule: updateSchedule,
-      addWorkOrderSchedule: addWorkOrderSchedule,
-      updateOrderProduct: updateOrderProduct,
-      getDatewiseEvents: getDatewiseEvents
-    };
-  }
-  initFactory.$inject = ["$q", "$cacheFactory", "api-base-factory", "shared-data-factory"];
-  angular.module("fpm").factory("work-orders-factory", initFactory);
+    initFactory.$inject = ["$q", "$cacheFactory", "api-base-factory", "shared-data-factory"];
+    angular.module("fpm").factory("work-orders-factory", initFactory);
 })();
