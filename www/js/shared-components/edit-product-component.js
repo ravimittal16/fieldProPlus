4b65d8c9e6e6665c4a3a5177a2553cf91f16ac19
Ravi Mittal <ravimittal16@gmail.com>
2018-06-17 17:39:19 +0530

    Resolved Price Issue

--- www/js/shared-components/edit-product-component.js
+++ www/js/shared-components/edit-product-component.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
   "use strict";
   var componentConfig = {
     bindings: {
@@ -18,7 +18,7 @@
       "shared-data-factory",
       "estimates-factory",
       "inventory-data-factory",
-      function (
+      function(
         $scope,
         $stateParams,
         $rootScope,
@@ -35,39 +35,45 @@
         vm.errors = [];
         var alerts = fpmUtilitiesFactory.alerts;
         vm.events = {
-          closeProductEditModal: function () {
+          closeProductEditModal: function() {
             $rootScope.$broadcast("$fpm:closeEditProductModal");
           },
-          updateProductClick: function () {
+          updateProductClick: function() {
             if (vm.modalType === 0) {
-              var promise = vm.isEstimate ?
-                estimatesFactory.updateProduct :
-                workOrdersFactory.updateProduct;
+              var promise = vm.isEstimate
+                ? estimatesFactory.updateProduct
+                : workOrdersFactory.updateProduct;
               if (vm.user.inventoryOn === true) {
                 inventoryDataFactory
                   .getProductContainers(vm.product.num)
-                  .then(function (response) {
+                  .then(function(response) {
                     if (
                       response != null &&
                       response.collection != null &&
                       response.collection.length > 0
                     ) {
                       vm.containers = response.collection;
-                      var container = _.filter(vm.containers, function (c) {
+                      var container = _.filter(vm.containers, function(c) {
                         return c.userId === vm.user.userEmail;
                       });
                       if (container) {
                         if (vm.product.qty > container[0].qoh) {
-                          vm.errors.push("Maximum quantity can be added is only " + container[0].qoh);
+                          vm.errors.push(
+                            "Maximum quantity can be added is only " +
+                              container[0].qoh
+                          );
                           return false;
                         } else {
                           vm.errors = [];
                           fpmUtilitiesFactory
                             .showLoading("updating product...")
-                            .then(function () {
+                            .then(function() {
                               promise(vm.product)
-                                .then(function (response) {
-                                  $scope.$emit("$fpm:operation:updateProduct", response);
+                                .then(function(response) {
+                                  $scope.$emit(
+                                    "$fpm:operation:updateProduct",
+                                    response
+                                  );
                                   alerts.alert(
                                     "Success",
                                     "Product has been updated successfully."
@@ -82,9 +88,9 @@
               } else {
                 fpmUtilitiesFactory
                   .showLoading("updating product...")
-                  .then(function () {
+                  .then(function() {
                     promise(vm.product)
-                      .then(function (response) {
+                      .then(function(response) {
                         $scope.$emit("$fpm:operation:updateProduct", response);
                         alerts.alert(
                           "Success",
@@ -94,7 +100,6 @@
                       .finally(fpmUtilitiesFactory.hideLoading);
                   });
               }
-
             } else {
               vm.product.barCode = $stateParams.barCode;
               vm.product.quantity = vm.product.qty;
@@ -103,27 +108,32 @@
               if (vm.user.inventoryOn === true) {
                 inventoryDataFactory
                   .getProductContainers(vm.product.num)
-                  .then(function (response) {
+                  .then(function(response) {
                     if (
                       response != null &&
                       response.collection != null &&
                       response.collection.length > 0
                     ) {
                       vm.containers = response.collection;
-                      var container = _.filter(vm.containers, function (c) {
+                      var container = _.filter(vm.containers, function(c) {
                         return c.userId === vm.user.userEmail;
                       });
                       if (container) {
                         if (vm.product.qty > container[0].qoh) {
-                          vm.errors.push("Maximum quantity can be added is only " + container[0].qoh);
+                          vm.errors.push(
+                            "Maximum quantity can be added is only " +
+                              container[0].qoh
+                          );
                           return false;
                         } else {
                           vm.errors = [];
-                          fpmUtilitiesFactory.showLoading().then(function () {
+                          fpmUtilitiesFactory.showLoading().then(function() {
                             workOrdersFactory
                               .addProduct(vm.product)
-                              .then(function (response) {
-                                if (angular.isFunction(vm.onAddProductCompleted)) {
+                              .then(function(response) {
+                                if (
+                                  angular.isFunction(vm.onAddProductCompleted)
+                                ) {
                                   vm.onAddProductCompleted({
                                     product: response
                                   });
@@ -141,10 +151,10 @@
                     }
                   });
               } else {
-                fpmUtilitiesFactory.showLoading().then(function () {
+                fpmUtilitiesFactory.showLoading().then(function() {
                   workOrdersFactory
                     .addProduct(vm.product)
-                    .then(function (response) {
+                    .then(function(response) {
                       if (angular.isFunction(vm.onAddProductCompleted)) {
                         vm.onAddProductCompleted({
                           product: response
@@ -159,17 +169,15 @@
                     .finally(fpmUtilitiesFactory.hideLoading);
                 });
               }
-
             }
           }
         };
 
-        vm.$onInit = function () {
-          sharedDataFactory.getIniitialData().then(function (response) {
+        vm.$onInit = function() {
+          sharedDataFactory.getIniitialData().then(function(response) {
             vm.enableMarkupOrders =
               response.customerNumberEntity.enableMarkupForWorkOrders || false;
           });
-          vm.user.showPrice = true;
         };
       }
     ],
