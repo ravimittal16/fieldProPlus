354f61d98afb96a11112835b880acfb04dc9c433
ravimittal16 <ravimittal16@gmail.com>
2019-11-09 01:36:14 +0530

    Updated Packages

--- www/js/shared-components/products-barcode-scanner-component/barcode-scanner-component.js
+++ www/js/shared-components/products-barcode-scanner-component/barcode-scanner-component.js
@@ -1,21 +1,24 @@
-(function() {
+(function () {
   "use strict";
   var componentConfig = {
     bindings: {
       onScanned: "&",
       buttonLabel: "@"
     },
-    template:
-      '<button class="button button-default button-small" ng-click="vm.doScan()">{{vm.buttonLabel}}</button>',
+    template: '<button class="button button-default button-small" ng-click="vm.doScan()">{{vm.buttonLabel}}</button>',
     controller: [
       "$cordovaBarcodeScanner",
-      function($cordovaBarcodeScanner) {
+      function ($cordovaBarcodeScanner) {
         var vm = this;
-        vm.doScan = function() {
-          $cordovaBarcodeScanner.scan().then(function(imageData) {
+        vm.doScan = function () {
+          $cordovaBarcodeScanner.scan({
+            disableAnimations: true
+          }).then(function (imageData) {
             if (imageData && imageData.text) {
               if (angular.isFunction(vm.onScanned)) {
-                vm.onScanned({ skuCode: imageData.text });
+                vm.onScanned({
+                  skuCode: imageData.text
+                });
               }
             }
           });
