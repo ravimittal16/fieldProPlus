dbadf1304bfbd15b2fac2be1fba972ea179345eb
Ravi Mittal <ravimittal16@gmail.com>
2020-11-08 22:45:56 +0530

    resolved error doc

--- www/js/work-orders/order-documents-list-component.js
+++ www/js/work-orders/order-documents-list-component.js
@@ -3,10 +3,19 @@
   var componentConfig = {
     templateUrl: "js/work-orders/order-documents-list-component.template.html",
     bindings: {
-      barcode: "<"
+      barcode: "<",
     },
-    controller: ["$scope", "work-orders-factory", "fpm-utilities-factory", "fieldPromaxConfig",
-      function ($scope, workOrdersFactory, fpmUtilitiesFactory, fieldPromaxConfig) {
+    controller: [
+      "$scope",
+      "work-orders-factory",
+      "fpm-utilities-factory",
+      "fieldPromaxConfig",
+      function (
+        $scope,
+        workOrdersFactory,
+        fpmUtilitiesFactory,
+        fieldPromaxConfig
+      ) {
         var vm = this;
         var baseUrl = fieldPromaxConfig.fieldPromaxApi;
         var alerts = fpmUtilitiesFactory.alerts;
@@ -14,27 +23,41 @@
         vm.gettinDocuments = false;
         vm.events = {
           onDocumentClicked: function (doc) {
-            window.open(baseUrl + "Handlers/GetImageFromBlob.ashx?imageId=" + doc.num + "&dateStamp=" + new Date() + "&flag=viewTarget&cust=" + doc.customerNumber, "_system", "location=yes");
-          }
+            cordova.InAppBrowser.open(
+              baseUrl +
+                "Handlers/GetImageFromBlob.ashx?imageId=" +
+                doc.num +
+                "&dateStamp=" +
+                new Date() +
+                "&flag=viewTarget&cust=" +
+                doc.customerNumber,
+              "_system",
+              "location=yes"
+            );
+          },
         };
         vm.$onInit = function () {
           if (vm.barcode) {
             vm.barcodeDocuments = [];
             vm.gettinDocuments = true;
-            workOrdersFactory.getUploadedDocuments(vm.barcode).then(function (response) {
-              if (angular.isArray(response) && response.length > 0) {
-                var pdfs = _.where(response, { extension: ".pdf" });
-                vm.barcodeDocuments = pdfs;
-              }
-              vm.gettinDocuments = false;
-            });
+            workOrdersFactory
+              .getUploadedDocuments(vm.barcode)
+              .then(function (response) {
+                if (angular.isArray(response) && response.length > 0) {
+                  var pdfs = _.where(response, { extension: ".pdf" });
+                  vm.barcodeDocuments = pdfs;
+                }
+                vm.gettinDocuments = false;
+              });
           } else {
             vm.gettinDocuments = false;
           }
-        }
-      }
+        };
+      },
     ],
-    controllerAs: "vm"
+    controllerAs: "vm",
   };
-  angular.module("fpm").component("orderDocumentsListComponent", componentConfig);
+  angular
+    .module("fpm")
+    .component("orderDocumentsListComponent", componentConfig);
 })();
