2ce4ad92ae4c3bd0c741d943fe7655fd27ea39c8
Ravi Mittal <ravimittal16@gmail.com>
2022-10-17 23:43:33 +0530

    Updated

--- www/js/work-orders/order-payment-component.js
+++ www/js/work-orders/order-payment-component.js
@@ -1,162 +1,192 @@
 (function () {
-  "use strict";
-  var componentConfig = {
-    bindings: {
-      barcode: "<",
-      totalAmountDue: "=",
-    },
-    templateUrl: "js/work-orders/order-payment-component.template.html",
-    controller: ["$scope", "$state", "$stateParams", "authenticationFactory", "work-orders-factory", "fieldPromaxConfig", "fpm-utilities-factory", "$timeout",
-      function ($scope, $state, $stateParams, authenticationFactory, workOrderFactory, fieldPromaxConfig, fpmUtilities, $timeout) {
-        var vm = this;
-        var baseUrl = fieldPromaxConfig.fieldPromaxApi;
-        var user = authenticationFactory.getLoggedInUserInfo();
-        vm.showList = false;
-        vm.paymentModal = null;
-        var alerts = fpmUtilities.alerts;
-        var paymentSchema = {
-          paymentMode: "",
-          receivedDate: new Date(),
-          barCode: vm.barcode,
-          paymentMemo: "",
-          amountReceived: 0.00,
-          serviceProviderEmail: ""
-        };
+    "use strict";
+    var componentConfig = {
+        bindings: {
+            barcode: "<",
+            totalAmountDue: "="
+        },
+        templateUrl: "js/work-orders/order-payment-component.template.html",
+        controller: [
+            "$scope",
+            "$state",
+            "$stateParams",
+            "authenticationFactory",
+            "work-orders-factory",
+            "fieldPromaxConfig",
+            "fpm-utilities-factory",
+            "$timeout",
+            function (
+                $scope,
+                $state,
+                $stateParams,
+                authenticationFactory,
+                workOrderFactory,
+                fieldPromaxConfig,
+                fpmUtilities,
+                $timeout
+            ) {
+                var vm = this;
+                var baseUrl = fieldPromaxConfig.fieldPromaxApi;
+                var user = authenticationFactory.getLoggedInUserInfo();
+                vm.showList = false;
+                vm.paymentModal = null;
+                var alerts = fpmUtilities.alerts;
+                var paymentSchema = {
+                    paymentMode: "",
+                    receivedDate: new Date(),
+                    barCode: vm.barcode,
+                    paymentMemo: "",
+                    amountReceived: 0.0,
+                    serviceProviderEmail: ""
+                };
 
-        function calculateBalanceDue() {
-          var amountReceived = 0;
-          var totalAmountDue = vm.totalAmountDue.toFixed(2);
-          angular.forEach(vm.payments, function (p, i) {
-            amountReceived += parseFloat(p.amountReceived);
-          });
+                function calculateBalanceDue() {
+                    var amountReceived = 0;
+                    var totalAmountDue = vm.totalAmountDue.toFixed(2);
+                    angular.forEach(vm.payments, function (p, i) {
+                        amountReceived += parseFloat(p.amountReceived);
+                    });
 
-          vm.amountReceived = amountReceived;
-          vm.balanceDue = totalAmountDue - amountReceived;
-        }
-
-        function getBarcodePayments() {
-          workOrderFactory
-            .getBarcodePayments(vm.barcode)
-            .then(
-              function (response) {
-                if (response != null) {
-                  vm.showList = true;
-                  vm.payments = angular.copy(response);
-                } else {
-                  vm.showList = false;
+                    vm.amountReceived = amountReceived;
+                    vm.balanceDue = totalAmountDue - amountReceived;
                 }
-                calculateBalanceDue();
-              });
-        }
 
-        function openAddEditPaymentModal() {
-          vm.modalType = 1;
-          vm.currentPayment = angular.copy(paymentSchema);
-          if (vm.paymentModal) {
-            vm.paymentModal.show();
-          } else if (vm.balanceDue > 0) {
-            fpmUtilities
-              .getModal("addEditPaymentModal.html", $scope)
-              .then(function (modal) {
-                vm.addEditPaymentModal = modal;
-                vm.addEditPaymentModal.show();
-              });
-          } else {
-            alerts.alert("Alert", "There is no amount to charge against this work order.", function () {
-              return false;
-            });
-          }
-        }
-        $scope.$on("$fpm:closeAddEditPaymentModal", function () {
-          if (vm.addEditPaymentModal) {
-            vm.addEditPaymentModal.hide();
-          }
-        });
+                function getBarcodePayments() {
+                    workOrderFactory
+                        .getBarcodePayments(vm.barcode)
+                        .then(function (response) {
+                            if (response != null) {
+                                vm.showList = true;
+                                vm.payments = angular.copy(response);
+                            } else {
+                                vm.showList = false;
+                            }
+                            calculateBalanceDue();
+                        });
+                }
+                /**
+                 * PAYMENT ADD MODAL IS NOT GETTING CLOSED
+                 */
+                function __closePaymentModal() {
+                    if (vm.addEditPaymentModal) {
+                        vm.addEditPaymentModal.hide();
+                        vm.addEditPaymentModal.remove();
+                        vm.addEditPaymentModal = null;
+                    }
+                }
 
-        $scope.$watch("vm.totalAmountDue", function (n, o) {
-          calculateBalanceDue();
-        });
+                function __updateBalanceList(closeModal) {
+                    $timeout(function () {
+                        vm.showList = true;
+                        calculateBalanceDue();
+                    }, 50);
+                    if (closeModal) {
+                        __closePaymentModal();
+                    }
+                }
 
-        $scope.$on("$fpm:addUpdatePaymentCompleted", function (event, data) {
-          vm.payments = angular.copy(data.o.payments);
-          if (data.o.payment.num == undefined) {
-            alerts.alert(
-              "Success",
-              "Payment added successfully",
-              function () {
-                $timeout(function () {
-                  vm.showList = true;
-                  calculateBalanceDue();
-                }, 50);
-              }
-            );
-          } else {
-            alerts.alert(
-              "Success",
-              "Payment updated successfully",
-              function () {
-                $timeout(function () {
-                  vm.showList = true;
-                  calculateBalanceDue();
-                }, 50);
-              }
-            );
-          }
-          vm.addEditPaymentModal.hide();
-        });
-        vm.events = {
-          collectPaymentClicked: function () {
-            if (vm.balanceDue > 0) {
-              $state.go('app.checkout', {
-                amount: vm.balanceDue,
-                barcode: vm.barcode,
-                technicianNum: $stateParams.technicianNum
-              });
-            } else {
-              alerts.alert("Alert", "There is no amount to charge against this work order.", function () {
-                return false;
-              });
-            }
-          },
-          addEditPayment: function () {
-            openAddEditPaymentModal();
-          },
-          onEditPaymentClicked: function (pmt) {
-            vm.modalType = 0;
-            vm.currentPayment = angular.copy(pmt);
-            vm.currentPayment.receivedDate = kendo.parseDate(pmt.receivedDate);
-            if (vm.paymentModal) {
-              vm.paymentModal.show();
-            } else {
-              fpmUtilities
-                .getModal("addEditPaymentModal.html", $scope)
-                .then(function (modal) {
-                  vm.addEditPaymentModal = modal;
-                  vm.addEditPaymentModal.show();
+                function openAddEditPaymentModal() {
+                    vm.modalType = 1;
+                    vm.currentPayment = angular.copy(paymentSchema);
+                    if (vm.paymentModal) {
+                        vm.paymentModal.show();
+                    } else if (vm.balanceDue > 0) {
+                        fpmUtilities
+                            .getModal("addEditPaymentModal.html", $scope)
+                            .then(function (modal) {
+                                __closePaymentModal();
+                            });
+                    } else {
+                        alerts.alert(
+                            "Alert",
+                            "There is no amount to charge against this work order.",
+                            function () {
+                                return false;
+                            }
+                        );
+                    }
+                }
+                $scope.$on("$fpm:closeAddEditPaymentModal", function () {
+                    __closePaymentModal();
                 });
-            }
-          },
-          onDeletePaymentClicked: function (pmt) {
-            alerts.confirmDelete(function () {
-              workOrderFactory
-                .deletePayment(pmt.num)
-                .then(function (response) {
-                  if (response) {
-                    vm.payments = response;
-                  }
-                  calculateBalanceDue();
+
+                $scope.$watch("vm.totalAmountDue", function (n, o) {
+                    calculateBalanceDue();
                 });
-            });
-          }
 
-        }
+                $scope.$on(
+                    "$fpm:addUpdatePaymentCompleted",
+                    function (event, data) {
+                        vm.payments = angular.copy(data.o.payments);
+                        alerts.alert(
+                            "Success",
+                            data.o.payment.num == undefined
+                                ? "Payment added successfully"
+                                : "Payment updated successfully",
+                            function () {
+                                __updateBalanceList(true);
+                            }
+                        );
+                    }
+                );
+                vm.events = {
+                    collectPaymentClicked: function () {
+                        if (vm.balanceDue > 0) {
+                            $state.go("app.checkout", {
+                                amount: vm.balanceDue,
+                                barcode: vm.barcode,
+                                technicianNum: $stateParams.technicianNum
+                            });
+                        } else {
+                            alerts.alert(
+                                "Alert",
+                                "There is no amount to charge against this work order.",
+                                function () {
+                                    return false;
+                                }
+                            );
+                        }
+                    },
+                    addEditPayment: function () {
+                        openAddEditPaymentModal();
+                    },
+                    onEditPaymentClicked: function (pmt) {
+                        vm.modalType = 0;
+                        vm.currentPayment = angular.copy(pmt);
+                        vm.currentPayment.receivedDate = kendo.parseDate(
+                            pmt.receivedDate
+                        );
+                        if (vm.paymentModal) {
+                            vm.paymentModal.show();
+                        } else {
+                            fpmUtilities
+                                .getModal("addEditPaymentModal.html", $scope)
+                                .then(function (modal) {
+                                    vm.addEditPaymentModal = modal;
+                                    vm.addEditPaymentModal.show();
+                                });
+                        }
+                    },
+                    onDeletePaymentClicked: function (pmt) {
+                        alerts.confirmDelete(function () {
+                            workOrderFactory
+                                .deletePayment(pmt.num)
+                                .then(function (response) {
+                                    if (response) {
+                                        vm.payments = response;
+                                    }
+                                    calculateBalanceDue();
+                                });
+                        });
+                    }
+                };
 
-        vm.$onInit = function () {
-          getBarcodePayments();
-        }
-      }
-    ],
-    controllerAs: "vm"
-  };
-  angular.module("fpm").component("orderPaymentComponent", componentConfig);
+                vm.$onInit = function () {
+                    getBarcodePayments();
+                };
+            }
+        ],
+        controllerAs: "vm"
+    };
+    angular.module("fpm").component("orderPaymentComponent", componentConfig);
 })();
